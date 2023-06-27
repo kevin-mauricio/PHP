2400cb0ee2ac374c8d239f0ac034f5879ff2eba3
kevin-mauricio <kmma407@gmail.com>
2023-06-26 23:37:12 -0500

    Agregando botones Modificar y Eliminar

--- 06 - Sistema Facturacion/consultar_producto.php
+++ 06 - Sistema Facturacion/consultar_producto.php
@@ -1,8 +1,6 @@
 <?php
 include_once("conexion.php");
-$consulta_productos = $conexion->query(
-    "SELECT * FROM producto"
-);
+$consulta_productos = $conexion->query("SELECT * FROM producto");
 
 ?>
 <!DOCTYPE html>
@@ -64,73 +62,75 @@ $consulta_productos = $conexion->query(
         </header>
 
         <main>
-            <div class="container">
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">NOMBRE</th>
-                        <th scope="col">PRECIO</th>
-                        <th scope="col">STOCK</th>
-                        <th scope="col">VER</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        if($consulta_productos->num_rows > 0) {
-                            while($row = $consulta_productos->fetch_assoc()) {
-                                $id = $row['id_producto'];
-                                $nombre = $row['nombre'];
-                                $id_proveedor = $row['id_proveedor'];
-                                $descripcion = $row['descripcion'];
-                                $costo = $row['costo'];
-                                $precio = $row['precio'];
-                                $stock = $row['stock'];
-
-                                ?>
-                                <tr>
-                                    <th scope="row"><?php echo $id; ?></th>
-                                    <td><?php echo $nombre ?></td>
-                                    <td><?php echo $precio ?></td>
-                                    <td><?php echo $stock ?></td>
-                                    <td>
-                                        <form action="" method="post">
-                                            <button class="btn btn-primary" type="button" name="detalles" value="<?php echo $id; ?>" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Detalles</button>
-                                            <?php
-                                        if (isset($_POST['detalles'])) {
-                                            $id = $_POST['detalles'];
-                                            // consultando el nombre del proveedor
-                                            $nombre_proveedor = $conexion->query(
-                                                "SELECT nombre FROM proveedor WHERE id_proveedor = $id_proveedor;"
-                                            );
-                                            ?>
-                                            <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-                                            <div class="offcanvas-header">
-                                                <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Detalles del producto</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <div class="container p-4">
+                <div class="col text-center">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">NOMBRE</th>
+                            <th scope="col">PRECIO</th>
+                            <th scope="col">STOCK</th>
+                            <th scope="col">VER</th>
+                            <th scope="col">ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            if($consulta_productos->num_rows > 0) {
+                                while($row = $consulta_productos->fetch_assoc()) {
+                                    $id = $row['id_producto'];
+                                    $nombre = $row['nombre'];
+                                    $id_proveedor = $row['id_proveedor'];
+                                    $descripcion = $row['descripcion'];
+                                    $costo = $row['costo'];
+                                    $precio = $row['precio'];
+                                    $stock = $row['stock'];
+    
+                                    // consultando el nombre del proveedor
+                                    $consulta_proveedor = $conexion->query("SELECT nombre FROM proveedor WHERE id_proveedor = $id_proveedor");
+                                    $proveedor = $consulta_proveedor->fetch_assoc();
+                                    $nombre_proveedor = $proveedor['nombre'];
+                                    ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $id; ?></th>
+                                        <td><?php echo $nombre; ?></td>
+                                        <td><?php echo $precio; ?></td>
+                                        <td><?php echo $stock; ?></td>
+                                        <td>
+                                            <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalDetalles<?php echo $id; ?>">Detalles</button>
+    
+                                            <!-- Modal para mostrar los detalles del producto -->
+                                            <div class="modal fade" id="modalDetalles<?php echo $id; ?>" tabindex="-1" aria-labelledby="modalDetallesLabel<?php echo $id; ?>" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="modalDetallesLabel<?php echo $id; ?>">Detalles del producto - <?php echo $nombre; ?></h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <p>Nombre: <?php echo $nombre; ?></p>
+                                                            <p>Id Producto: <?php echo $id; ?></p>
+                                                            <p>Proveedor: <?php echo $nombre_proveedor; ?></p>
+                                                            <p>Descripción: <?php echo $descripcion; ?></p>
+                                                            <p>Costo: <?php echo $costo; ?></p>
+                                                            <p>Precio: <?php echo $precio; ?></p>
+                                                            <p>Stock: <?php echo $stock; ?></p>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div class="offcanvas-body">
-                                                <p>Nombre: <?php echo $nombre ?></p>
-                                                <p>ID: <?php echo $id ?></p>
-                                                <p>Proveedor: <?php echo $id_proveedor; ?></p>
-                                                <p>Descripción: <?php echo $descripcion ?></p>
-                                                <p>Costo: <?php echo $costo ?></p>
-                                                <p>Precio: <?php echo $precio ?></p>
-                                                <p>Stock: <?php echo $stock ?></p>
-                                            </div>
-                                            </div>
-                                            <?php
-                                        }
-                                        ?>
-                                    </form>
-                                </td>
-                                </tr>
+                                        </td>
+                                        <td>
+                                            <a class="btn btn-success m-1" href="modificar.php?id<?=$id; ?>">Modificar</a><a class="btn btn-danger" href="">Eliminar</a>
+                                        </td>
+                                    </tr>
                                 <?php
-                            }
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                                }
+                            } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </main>
 
@@ -140,6 +140,7 @@ $consulta_productos = $conexion->query(
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://kit.fontawesome.com/ca748a8320.js" crossorigin="anonymous"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
     </body>
