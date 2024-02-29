3ef6215bd3e06f1e087f083835fb991aa9b633be
kevin-mauricio <kmma407@gmail.com>
2024-02-29 11:39:45 -0500

    migraciones y restaurante colinas

---
+++ 10 - Colinas restaurante/colinas_restaurante/database/migrations/2024_02_29_142623_create_platos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('platos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+        
+            // Grupo de columnas relacionadas con precios
+            $table->float('precio');
+            $table->float('costo');
+        
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('platos');
+    }
+};
