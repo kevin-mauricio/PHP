0418874feb83e4635a19d0c47478d0a3eaa2ee3e
kevin-mauricio <kmma407@gmail.com>
2024-03-18 11:35:48 -0500

    relations

---
+++ 13 - Relations/relations/database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'id' => 1,
+            'name' => 'example',
+            'email' => 'example@example.com',
+            'password' => '12345678'
+        ]);
+        User::create([
+            'id' => 2,
+            'name' => 'example 2',
+            'email' => 'example2@example.com',
+            'password' => '12345678'
+        ]);
+        User::create([
+            'id' => 3,
+            'name' => 'example 3',
+            'email' => 'example3@example.com',
+            'password' => '12345678'
+        ]);
+    }
+}
